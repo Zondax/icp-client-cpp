be8cf5da9806c15cccb125308080a55a5f7b182f
Carlos Medeiros <carlos.medeiros@zondax.ch>
2023-03-31 12:40:33 +0100

    rename and clean some stuff

--- c-playgroud/build.sh
+++ c-playgroud/build.sh
@@ -1,6 +1,6 @@
-cp ../ic-agent-ffi/bindings.h test.h
+cp ../ic-agent-wrapper/bindings.h test.h
 
 gcc -o test \
-    -L ../ic-agent-ffi/target/release/ -lic_agent_ffi\
+    -L ../ic-agent-wrapper/target/release/ -lic_agent_wrapper\
     -I - \
     test.c 
---
+++ ic-agent-wrapper/src/request_id/mod.rs
@@ -0,0 +1,30 @@
+use ic_agent::request_id::RequestId;
+
+/// Creates a new RequestId from a SHA-256 hash.
+#[no_mangle]
+pub extern "C" fn request_id_new(hash:  &[u8; 32]) -> *mut RequestId {
+    Box::into_raw(Box::new(RequestId::new(hash)))
+}
+
+/// Free created RequestId
+#[no_mangle]
+pub extern "C" fn request_id_free(client: *mut RequestId) {
+    if !client.is_null() {
+        unsafe {
+            drop(Box::from_raw(client));
+        }
+    }
+}
+
+/// Returns the SHA-256 hash this ID is based on.
+#[no_mangle]
+pub extern "C" fn request_id_as_slice(ptr: *mut RequestId) ->  *const u8 {
+    let mpc = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+    mpc.as_slice().as_ptr()
+}
+
+// TODO : to_request_id
+// TODO : TESTING
