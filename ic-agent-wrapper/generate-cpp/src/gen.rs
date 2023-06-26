71e79c25cf215c37011c88977eabfc86597bc67e
becominginsane <francesco@zondax.ch>
2023-06-27 01:52:20 +0800

    feat(generate-cpp): generate C++ from candid

---
+++ ic-agent-wrapper/generate-cpp/src/gen.rs
@@ -0,0 +1,515 @@
+use std::collections::BTreeSet;
+
+/// Compile the given type declarations into C++ bindings
+pub fn compile(env: &TypeEnv, actor: &Option<Type>) -> String {
+    let header = r#"// This is an experimental feature to generate C++ bindings from Candid.
+// You may want to manually adjust some of the types.
+
+#include <optional>
+#include <stdint.h>
+#include <string>
+#include <variant>
+#include <vector>
+"#;
+
+    let (env, actor) = nominalize_all(env, actor);
+    let def_list: Vec<_> = if let Some(actor) = &actor {
+        chase_actor(&env, actor).unwrap()
+    } else {
+        env.0.iter().map(|pair| pair.0.as_ref()).collect()
+    };
+
+    let recs = infer_rec(&env, &def_list).unwrap();
+    let defs = pp_defs(&env, &def_list, &recs);
+    let doc = match &actor {
+        None => defs,
+        Some(_actor) => {
+            // let actor = pp_actor(&env, actor);
+            // defs.append(actor)
+            defs
+        }
+    };
+    let doc = RcDoc::text(header).append(RcDoc::line()).append(doc);
+    doc.pretty(LINE_WIDTH).to_string()
+}
+static KEYWORDS: [&str; 127] = [
+    "alignas",
+    "alignof",
+    "and",
+    "and_eq",
+    "asm",
+    "atomic_cancel",
+    "atomic_commit",
+    "atomic_noexcept",
+    "auto",
+    "bitand",
+    "bitor",
+    "bool",
+    "break",
+    "case",
+    "catch",
+    "char",
+    "char8_t",
+    "char16_t",
+    "char32_t",
+    "class",
+    "compl",
+    "concept",
+    "const",
+    "consteval",
+    "constexpr",
+    "constinit",
+    "const_cast",
+    "continue",
+    "co_await",
+    "co_return",
+    "co_yield",
+    "decltype",
+    "default",
+    "delete",
+    "do",
+    "double",
+    "dynamic_cast",
+    "else",
+    "enum",
+    "explicit",
+    "export",
+    "extern",
+    "false",
+    "float",
+    "for",
+    "friend",
+    "goto",
+    "if",
+    "inline",
+    "int",
+    "long",
+    "mutable",
+    "namespace",
+    "new",
+    "noexcept",
+    "not",
+    "not_eq",
+    "nullptr",
+    "operator",
+    "or",
+    "or_eq",
+    "private",
+    "protected",
+    "public",
+    "reflexpr",
+    "register",
+    "reinterpret_cast",
+    "requires",
+    "return",
+    "short",
+    "signed",
+    "sizeof",
+    "static",
+    "static_assert",
+    "static_cast",
+    "struct",
+    "switch",
+    "synchronized",
+    "template",
+    "this",
+    "thread_local",
+    "throw",
+    "true",
+    "try",
+    "typedef",
+    "typeid",
+    "typename",
+    "union",
+    "unsigned",
+    "using",
+    "virtual",
+    "void",
+    "volatile",
+    "wchar_t",
+    "while",
+    "xor",
+    "xor_eq",
+    "final",
+    "override",
+    "transaction_safe",
+    "transaction_safe_dynamic",
+    "import",
+    "module",
+    "posix",
+    "if",
+    "elif",
+    "else",
+    "endif",
+    "ifdef",
+    "ifndef",
+    "elifdef",
+    "elifndef",
+    "define",
+    "undef",
+    "include",
+    "line",
+    "error",
+    "warning",
+    "pragma",
+    "defined",
+    "__has_include",
+    "__has_cpp_attribute",
+    "export",
+    "import",
+    "module",
+    "_Pragma",
+    "std",
+];
+
+fn ident_(id: &str) -> (String, bool) {
+    if id.is_empty()
+        || id.starts_with(|c: char| !c.is_ascii_alphabetic() && c != '_')
+        || id.chars().any(|c| !c.is_ascii_alphanumeric() && c != '_')
+        //reserved
+        || id.contains("__")
+        || KEYWORDS.contains(&id)
+        //global namespace reserved items
+        || id
+            .as_bytes()
+            .windows(2)
+            .take(1) //only check the start of the identifier
+            .any(|id| id[0] == b'_' && id[1].is_ascii_uppercase())
+    {
+        (format!("_{}_", idl_hash(id)), true)
+    } else {
+        (id.to_string(), false)
+    }
+}
+fn ident(id: &str) -> RcDoc {
+    RcDoc::text(ident_(id).0)
+}
+
+type RecPoints<'a> = BTreeSet<&'a str>;
+
+fn pp_label(id: &Label) -> RcDoc {
+    match id {
+        Label::Named(str) => ident(str),
+        Label::Id(n) | Label::Unnamed(n) => str("_").append(RcDoc::as_string(n)).append("_"),
+    }
+}
+
+fn pp_ty<'a, 'b>(ty: &'a Type, recs: &'b RecPoints) -> RcDoc<'a> {
+    use Type::*;
+    match *ty {
+        Null => str("void"),
+        Bool => str("bool"),
+        Nat => str("void* /* BigNat */"),
+        Int => str("void* /* BigInt */"),
+        Nat8 => str("uint8_t"),
+        Nat16 => str("uint16_t"),
+        Nat32 => str("uint32_t"),
+        Nat64 => str("uint64_t"),
+        Int8 => str("int8_t"),
+        Int16 => str("int16_t"),
+        Int32 => str("int32_t"),
+        Int64 => str("int64_t"),
+        Float32 => str("float"),
+        Float64 => str("double"),
+        Text => str("std::string"),
+        Reserved => str("void* /* candid::Reserved */"),
+        Empty => str("void* /* candid::Empty */"),
+        Var(ref id) => {
+            let name = ident(id);
+            if recs.contains(id.as_str()) {
+                str("std::unique_ptr<").append(name).append(">")
+            } else {
+                name
+            }
+        }
+        Principal => str("Principal"),
+        Opt(ref t) => str("std::optional").append(enclose("<", pp_ty(t, recs), ">")),
+        Vec(ref t) => str("std::vector").append(enclose("<", pp_ty(t, recs), ">")),
+        Record(ref fs) => pp_record_fields(fs, recs),
+        Variant(_) => unreachable!(), // not possible after rewriting
+        Func(_) => str("void* /* candid::Func */"),
+        Service(_) => str("void* /* candid::Service */"),
+        Class(_, _) => unreachable!(),
+        Knot(_) | Unknown => unreachable!(),
+    }
+}
+
+fn pp_record_field<'a, 'b>(field: &'a Field, recs: &'b RecPoints) -> RcDoc<'a> {
+    pp_ty(&field.ty, recs)
+        .append(" ")
+        .append(pp_label(&field.id))
+        .append(";")
+        .append(RcDoc::line_())
+}
+
+fn pp_record_fields<'a, 'b>(fs: &'a [Field], recs: &'b RecPoints) -> RcDoc<'a> {
+    if is_tuple(fs) {
+        strict_concat(fs.iter().map(|f| pp_ty(&f.ty, recs)), ",")
+    } else {
+        RcDoc::concat(fs.iter().map(|f| pp_record_field(f, recs)))
+    }
+}
+
+fn pp_record<'a, 'b>(id: &'a str, fs: &'a [Field], recs: &'b RecPoints) -> RcDoc<'a> {
+    let name = str(id).append(" ");
+
+    let pp_mby_unit_record_constructor = || {
+        if fs.is_empty() && !is_tuple(fs) {
+            // avoids `{}` being a valid value of the struct
+            // so instead you should use Name{}
+            kwd("explicit").append(id).append("() = default;")
+        } else {
+            RcDoc::nil()
+        }
+    };
+
+    // let pp_mby_unit_record_constexpr = || {
+    //     if fs.is_empty() && !is_tuple(fs) {
+    //         kwd("inline constexpr")
+    //             .append(name.clone())
+    //             .append(name.clone())
+    //             .append(";")
+    //     } else {
+    //         RcDoc::line_()
+    //     }
+    // };
+
+    kwd("struct")
+        .append(name.clone())
+        .append(enclose(
+            "{",
+            pp_mby_unit_record_constructor().append(pp_record_fields(fs, recs)),
+            "}",
+        ))
+        .append(";")
+        // .append(pp_mby_unit_record_constexpr())
+        .append(RcDoc::hardline())
+}
+
+fn pp_variant_field<'a, 'b>(field: &'a Field, recs: &'b RecPoints) -> RcDoc<'a> {
+    match &field.ty {
+        // Type::Null => pp_label(&field.id),
+        // Type::Record(fs) => pp_label(&field.id).append(pp_record_fields(fs, recs)),
+        _ => pp_ty(&field.ty, recs),
+    }
+}
+
+fn pp_variant_fields<'a, 'b>(fs: &'a [Field], recs: &'b RecPoints) -> RcDoc<'a> {
+    strict_concat(fs.iter().map(|f| pp_variant_field(f, recs)), ",")
+}
+
+fn pp_defs<'a>(env: &'a TypeEnv, def_list: &'a [&'a str], recs: &'a RecPoints) -> RcDoc<'a> {
+    lines(def_list.iter().map(|id| {
+        let ty = env.find_type(id).unwrap();
+        let name = ident(id).append(" ");
+        match ty {
+            Type::Record(fs) if is_tuple(fs) => kwd("using")
+                .append(name)
+                .append("= std::tuple")
+                .append(enclose("<", pp_record_fields(fs, recs), ">;"))
+                .append(RcDoc::hardline()),
+            Type::Record(fs) => pp_record(id, fs, recs),
+            Type::Variant(fs) => kwd("using")
+                .append(name)
+                .append("= std::variant")
+                .append(enclose("<", pp_variant_fields(fs, recs), ">;"))
+                .append(RcDoc::hardline()),
+            _ => {
+                // if recs.contains(id) {
+                //TODO: newtype Foo(FooTy)
+                // kwd("struct")
+                //     .append(ident(id))
+                //     .append(enclose("(", pp_ty(ty, recs), ")"))
+                //     .append(";")
+                //     .append(RcDoc::hardline())
+                // } else {
+                kwd("using")
+                    .append(name)
+                    .append("= ")
+                    .append(pp_ty(ty, recs))
+                    .append(";")
+                // }
+            }
+        }
+    }))
+}
+
+/// The definition of tuple is language specific.
+///
+/// We'll be converting Unnamed records and the like into `std::tuple`
+pub(crate) fn is_tuple(fs: &[Field]) -> bool {
+    if fs.is_empty() {
+        return false;
+    }
+    !fs.iter()
+        .enumerate()
+        .any(|(i, field)| field.id.get_id() != (i as u32))
+}
+
+fn path_to_var(path: &[TypePath]) -> String {
+    let name: Vec<&str> = path
+        .iter()
+        .map(|node| match node {
+            TypePath::Id(id) => id.as_str(),
+            TypePath::RecordField(f) | TypePath::VariantField(f) => f.as_str(),
+            TypePath::Opt => "inner",
+            TypePath::Vec => "item",
+            TypePath::Func(id) => id.as_str(),
+            TypePath::Init => "init",
+        })
+        .collect();
+    name.join("_")
+}
+
+/// Convert structural typing to nominal typing to fit C++'s type system
+fn nominalize(env: &mut TypeEnv, path: &mut Vec<TypePath>, t: Type) -> Type {
+    match t {
+        Type::Opt(ty) => {
+            path.push(TypePath::Opt);
+            let ty = nominalize(env, path, *ty);
+            path.pop();
+            Type::Opt(Box::new(ty))
+        }
+        Type::Vec(ty) => {
+            path.push(TypePath::Opt);
+            let ty = nominalize(env, path, *ty);
+            path.pop();
+            Type::Vec(Box::new(ty))
+        }
+        Type::Record(fs) => {
+            if matches!(
+                path.last(),
+                None | Some(TypePath::VariantField(_) | TypePath::Id(_))
+            ) || is_tuple(&fs)
+            {
+                let fs: Vec<_> = fs
+                    .into_iter()
+                    .map(|Field { id, ty }| {
+                        path.push(TypePath::RecordField(id.to_string()));
+                        let ty = hoist_nested_type(env, path, id.clone(), ty);
+                        let ty = nominalize(env, path, ty);
+                        path.pop();
+                        Field { id, ty }
+                    })
+                    .collect();
+                Type::Record(fs)
+            } else {
+                let new_var = path_to_var(path);
+                let ty = nominalize(
+                    env,
+                    &mut vec![TypePath::Id(new_var.clone())],
+                    Type::Record(fs),
+                );
+                env.0.insert(new_var.clone(), ty);
+                Type::Var(new_var)
+            }
+        }
+        Type::Variant(fs) => match path.last() {
+            None | Some(TypePath::Id(_)) => {
+                let fs: Vec<_> = fs
+                    .into_iter()
+                    .map(|Field { id, ty }| {
+                        path.push(TypePath::VariantField(id.to_string()));
+                        let ty = hoist_nested_type(env, path, id.clone(), ty);
+                        let ty = nominalize(env, path, ty);
+                        path.pop();
+                        Field { id, ty }
+                    })
+                    .collect();
+                Type::Variant(fs)
+            }
+            Some(_) => {
+                let new_var = path_to_var(path);
+                let ty = nominalize(
+                    env,
+                    &mut vec![TypePath::Id(new_var.clone())],
+                    Type::Variant(fs),
+                );
+                env.0.insert(new_var.clone(), ty);
+                Type::Var(new_var)
+            }
+        },
+        Type::Func(func) => Type::Func(Function {
+            modes: func.modes,
+            args: func
+                .args
+                .into_iter()
+                .enumerate()
+                .map(|(i, ty)| {
+                    path.push(TypePath::Func(format!("arg{}", i)));
+                    let ty = nominalize(env, path, ty);
+                    path.pop();
+                    ty
+                })
+                .collect(),
+            rets: func
+                .rets
+                .into_iter()
+                .enumerate()
+                .map(|(i, ty)| {
+                    path.push(TypePath::Func(format!("ret{}", i)));
+                    let ty = nominalize(env, path, ty);
+                    path.pop();
+                    ty
+                })
+                .collect(),
+        }),
+        Type::Service(serv) => Type::Service(
+            serv.into_iter()
+                .map(|(meth, ty)| {
+                    path.push(TypePath::Id(meth.to_string()));
+                    let ty = nominalize(env, path, ty);
+                    path.pop();
+                    (meth, ty)
+                })
+                .collect(),
+        ),
+        Type::Class(args, ty) => Type::Class(
+            args.into_iter()
+                .map(|ty| {
+                    path.push(TypePath::Init);
+                    let ty = nominalize(env, path, ty);
+                    path.pop();
+                    ty
+                })
+                .collect(),
+            Box::new(nominalize(env, path, *ty)),
+        ),
+        _ => t,
+    }
+}
+
+/// Hoist a nested type into a separate record to be used
+fn hoist_nested_type(env: &mut TypeEnv, path: &mut Vec<TypePath>, label: Label, ty: Type) -> Type {
+    let mut id = ident_(&path_to_var(path)).0;
+    match ty {
+        Type::Null => {
+            env.0.insert(id.clone(), Type::Record(vec![]));
+            Type::Var(id)
+        }
+        record @ Type::Record(_) => {
+            env.0.insert(id.clone(), record);
+            Type::Var(id)
+        }
+        variant @ Type::Variant(_) => {
+            env.0.insert(id.clone(), variant);
+            Type::Var(id)
+        }
+        _ => ty,
+    }
+}
+
+/// Nominalize an entire environment and actor (if provided)
+fn nominalize_all(env: &TypeEnv, actor: &Option<Type>) -> (TypeEnv, Option<Type>) {
+    let mut res = TypeEnv(Default::default());
+    for (id, ty) in env.0.iter() {
+        let ty = nominalize(&mut res, &mut vec![TypePath::Id(id.clone())], ty.clone());
+        res.0.insert(id.to_string(), ty);
+    }
+
+    let actor = actor
+        .as_ref()
+        .map(|ty| nominalize(&mut res, &mut vec![], ty.clone()));
+
+    (res, actor)
+}
